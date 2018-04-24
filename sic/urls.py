49cfd70918132b094cb1b1115d67aeab7c150890
wowanamatsu <antonioflavionte@gmail.com>
2018-04-24 18:31:05 -0300

    Ajustes nas urls, adicionado o name

--- sic/urls.py
+++ sic/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pessoas/', views.index),
-    path('cadastros/', views.cadastro),
+    path('pessoas/', views.index, name='pessoas'),
+    path('cadastros/', views.cadastro, name='cadastro_pessoas'),
 ]
